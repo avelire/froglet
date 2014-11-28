38b13fe37e8e7ae964226f8d00b1771ce3cf5601
Val Ng <avelire@gmail.com>
2014-11-28 17:31:12 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newfangler::Application.config.secret_key_base = '7c7d2ec6a8c150fe6cd46559b6f1eab4da197f51ea0fef238df386afbe34925c9a361e2a5d535f884c73e22a8b74241eb6e84d5ed368125f5f10a1c44b4d2034'
